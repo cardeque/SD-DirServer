21503f2bc4792ccedbcc620d6882405809a9118c
Cardeque Borges <cardequeh@gmail.com>
2021-06-11 19:27:55 -0300

    .

--- clientCripto.py
+++ clientCripto.py
@@ -1,32 +1,62 @@
-import rpyc
+# import rpyc
+#
+# from constRPYC import *
+#
+# class Client:
+#     conn_directory = rpyc.connect(DIR_SERVER, DIR_PORT)
+#     (address, port) = conn_directory.root.exposed_lookup("ServerCrypt")
+#
+#     if address == 'error':
+#         print(port)
+#     else:
+#         print(f"Conexão: {address}:{port}")
+#         conn_server = rpyc.connect(address, port)
+#         operation = str(input("[1] Encriptar\n[2] Decifrar\n[3] Cancelar\n"))
+#
+#         if operation == "1" or operation == "2":
+#             if operation == "1":
+#                 text = input("Mensagem: ")
+#                 (encText, pub_key) = conn_server.root.exposed_encrypt(text)
+#                 print(f"Mensagem encriptada: {encText}\n Chave pública: {pub_key}")
+#                 file = open("enc_message.txt", 'w')
+#                 file.write(encText)
+#                 file.close()
+#             else:
+#                 # text = input("Encripted message: ")
+#                 msg = input("Entre a mensagem:")
+#                 print(f"Mensagem: {msg}")
+#                 pub_key = input("Chave publica: ")
+#                 text = conn_server.root.exposed_decrypt(msg, pub_key)
+#                 print(f"Mensagem: {text}")
+#         else:
+#             print("Adeus!")
 
+
+import rpyc
 from constRPYC import *
 
 class Client:
     conn_directory = rpyc.connect(DIR_SERVER, DIR_PORT)
     (address, port) = conn_directory.root.exposed_lookup("ServerCrypt")
-
     if address == 'error':
         print(port)
     else:
         print(f"Conexão: {address}:{port}")
         conn_server = rpyc.connect(address, port)
-        operation = str(input("[1] Encriptar\n[2] Decifrar\n[3] Cancelar\n"))
-
+        operation = str(input("Encriptar[1]:\nDecifrar[2]:\nCancelar[3]:\n"))
         if operation == "1" or operation == "2":
             if operation == "1":
                 text = input("Mensagem: ")
                 (encText, pub_key) = conn_server.root.exposed_encrypt(text)
-                print(f"Mensagem encriptada: {encText}\n Chave pública: {pub_key}")
+                print(f"Encriptada:{encText}\n Chave publica: {pub_key}")
                 file = open("enc_message.txt", 'w')
                 file.write(encText)
                 file.close()
             else:
-                # text = input("Encripted message: ")
-                msg = input("Entre a mensagem:")
-                print(f"Mensagem: {msg}")
-                pub_key = input("Chave publica: ")
-                text = conn_server.root.exposed_decrypt(msg, pub_key)
+                enc_message = input("Entre a mensagem encriptada: ")
+                print(f"Mensagem encriptada: {enc_message}")
+                pub_key = input("Chave Publica:")
+                text = conn_server.root.exposed_decrypt(enc_message, pub_key)
                 print(f"Mensagem: {text}")
         else:
             print("Adeus!")
--- serverCripto.py
+++ serverCripto.py
@@ -1,3 +1,66 @@
+# from random import randint
+# import rpyc
+# from socket import gethostbyname, gethostname
+# from constRPYC import *
+# from rpyc.utils.server import ThreadedServer
+#
+#
+# class ServerCrypt(rpyc.Service):  # alterar ServerCrypt para o nome do servidor
+#     priv_key = 1
+#
+#     def __encrypt(self, mensagem, pub_key):
+#         value = ''
+#         for item in mensagem:
+#             value = value + chr((ord(item) + pub_key + self.priv_key))
+#         return value
+#
+#     def __decrypt(self, mensagem, pub_key):
+#         value = ''
+#         for item in mensagem:
+#             value = value + chr((ord(item) - int(pub_key) - self.priv_key))
+#         return value
+#
+#     def exposed_encrypt(self, text):
+#         pub_key = randint(1, 25)
+#         enc_message = self.__encrypt(text, pub_key)
+#         return (enc_message, pub_key)
+#
+#     def exposed_decrypt(self, message, pub_key):
+#         return self.__decrypt(message, pub_key)
+#
+#
+# if __name__ == "__main__":
+#     server = ThreadedServer(ServerCrypt, port=20203)  # alterar o ServerCrypt para o nome do servidor
+#     conn = rpyc.connect(host=DIR_SERVER, port=DIR_PORT)
+#     my_address = gethostbyname(gethostname())
+#     (reg, token) = conn.root.exposed_register("Server Cripto", my_address, 20203)
+#     # print (reg) #precisa ser alterado para receber o token/senha de alteração
+#     if reg:
+#         print(f"Primeira conexão. Token: {token}")
+#         server.start()
+#     else:
+#         print("Reiniciando")
+#         option = str(input("1) Update.\n2) Remover serviço.\n3) Cancelar\n"))
+#         if option == "1" or option == "2":
+#             if option == "1":
+#                 token = str(input("Token: "))
+#                 update = conn.root.exposed_update_register("Server Cripto", my_address, 20203, token)
+#                 if update:
+#                     print("Atualizado")
+#                     server.start()
+#                 else:
+#                     print("Token invalido")
+#             else:
+#                 token = str(input("Token: "))
+#                 remove = conn.root.exposed_unregister("Server Cripto", token)
+#                 if remove:
+#                     print("Serviço removido")
+#                 else:
+#                     print("Token não autorizado")
+#         else:
+#             print("Adeus!")
+
+
 from random import randint
 import rpyc
 from socket import gethostbyname, gethostname
@@ -33,29 +96,29 @@ if __name__ == "__main__":
     server = ThreadedServer(ServerCrypt, port=20203)  # alterar o ServerCrypt para o nome do servidor
     conn = rpyc.connect(host=DIR_SERVER, port=DIR_PORT)
     my_address = gethostbyname(gethostname())
-    (reg, token) = conn.root.exposed_register("Server Cripto", my_address, 20203)
+    (reg, token) = conn.root.exposed_register("ServerCrypt", my_address, 20203)
     # print (reg) #precisa ser alterado para receber o token/senha de alteração
     if reg:
         print(f"Primeira conexão. Token: {token}")
         server.start()
     else:
         print("Reiniciando")
-        option = str(input("1) Update.\n2) Remover serviço.\n3) Cancelar\n"))
+        option = str(input("Atualizar serviço [1]\nRemover seviço [2]\nCancelar [3]\n"))
         if option == "1" or option == "2":
             if option == "1":
                 token = str(input("Token: "))
-                update = conn.root.exposed_update_register("Server Cripto", my_address, 20203, token)
+                update = conn.root.exposed_update_register("ServerCrypt", my_address, 20203, token)
                 if update:
-                    print("Atualizado")
+                    print("Atualizado.")
                     server.start()
                 else:
-                    print("Token invalido")
+                    print("Token não authorizado")
             else:
                 token = str(input("Token: "))
-                remove = conn.root.exposed_unregister("Server Cripto", token)
+                remove = conn.root.exposed_unregister("ServerCrypt", token)
                 if remove:
                     print("Serviço removido")
                 else:
-                    print("Token não autorizado")
+                    print("Token not authorized")
         else:
-            print("Adeus!")
+            print("Good bye!")
